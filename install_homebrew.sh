34692e46383830b86e44e25c26b0aeb797ceff6c
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-04-18 00:16:56 +0200

    not needed anymore

--- install_homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(command -v brew) == "" ]]; then
-  echo "🔧 Installing Homebrew ..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-echo "📦 Installing Brew packages..."
-brew bundle --file=./Brewfile
