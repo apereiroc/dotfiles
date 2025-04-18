c56d53a9a6f17198e76ecd6245fbf995322dc817
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-04-18 12:29:58 +0200

    add log in case homebrew is already installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@
 if [[ $(command -v brew) == "" ]]; then
   echo "🔧 Installing Homebrew ..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Found a Homebrew installation!"
 fi
 
 echo "📦 Installing Brew packages..."
