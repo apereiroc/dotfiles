dac4a451965c7ddb32b521a6219541ab3545fab7
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-04-19 16:02:37 +0200

    improve symlink and add zsh

--- symlink.sh
+++ symlink.sh
@@ -2,18 +2,29 @@
 
 echo "Linking dotfiles..."
 
-echo "    Linking nvim ..."
-mkdir -p $HOME/.config/nvim
-stow --restow --target=$HOME/.config/nvim nvim
+# stow limitation ...
+cd $HOME/.dotfiles
+# get command_exists
+source zsh/functions.zsh
 
-echo "    Syncing ..."
-nvim --headless +"Lazy! sync" +qa
+if command_exists nvim; then
+  echo "    Linking nvim ..."
+  mkdir -p $HOME/.config/nvim
+  stow --restow --target=$HOME/.config/nvim nvim
 
-echo "    Linking kitty ..."
-mkdir -p $HOME/.config/kitty
-stow --restow --target=$HOME/.config/kitty kitty
+  echo "    Syncing ..."
+  nvim --headless +"Lazy! sync" +qa
+fi
+
+if command_exists kitty; then
+  echo "    Linking kitty ..."
+  mkdir -p $HOME/.config/kitty
+  stow --restow --target=$HOME/.config/kitty kitty
+fi
 
 echo "    Linking zsh ..."
-stow --restow --target=$HOME zsh
+if command_exists zsh; then
+  stow --restow --target=$HOME zsh
+fi
 
 echo "Dotfiles symlinked."
